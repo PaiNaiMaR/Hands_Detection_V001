efc57728ca77a301067490b400f3bdac5e995d2a
DDream <117961741+PaiNaiMaR@users.noreply.github.com>
2023-02-22 18:07:15 +0700

    Update Hands_Detection.py

--- Hands_Detection.py
+++ Hands_Detection.py
@@ -1,19 +1,28 @@
+"""
+ Hand Detection
+ Mr.Tawatchai Poungmanee
+ Class-Audio@Outlook.co.th
+ Library
+ : opencv2
+ : mediapipe
+"""
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For webcam input:
+# SET webcam input:
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
   print("can open camera")
 else:
-   cap = cv2.VideoCapture(cv2.CAP_V4L2)
+  cap = cv2.VideoCapture(cv2.CAP_V4L2)
+  print("can open camera")
 
 with mp_hands.Hands(
   model_complexity=0,
-  min_detection_confidence=0.5,
+  min_detection_confidence=0.6,  # 0 - 1 
   min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
     success, image = cap.read()
@@ -29,7 +38,10 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
     fingerCount = 0
-
+    L = 0
+    R = 0
+    H = 1
+    
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         handIndex = results.multi_hand_landmarks.index(hand_landmarks)
@@ -39,7 +51,7 @@ with mp_hands.Hands(
 
         for landmarks in hand_landmarks.landmark:
           handLandmarks.append([landmarks.x, landmarks.y])
-        print(handLabel)
+
         if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
           fingerCount = fingerCount + 1
         elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
@@ -61,10 +73,26 @@ with mp_hands.Hands(
           mp_drawing_styles.get_default_hand_landmarks_style(),
           mp_drawing_styles.get_default_hand_connections_style()
         )
+        if handLabel == "Left":
+          L = 1
+        elif handLabel == "Right":
+          R = 2
+          
+    #Hand L - R
+    H = L + R 
+    if H == 1:
+      cv2.putText(image, "Left", (500,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
+    elif H == 2:
+      cv2.putText(image, "Right", (500,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
+    elif H == 3:
+      cv2.putText(image, "Left-Right", (500,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 2)
 
-    cv2.putText(image, str(fingerCount), (50,450), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (255,0,0), 10)
+    if results.multi_hand_landmarks == None:
+        H = 0 
+    
+    cv2.putText(image, str(fingerCount), (20,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,255), 3)
     cv2.imshow("FingerCounting Apps", image)
-
+    #Exit Key ESC
     if cv2.waitKey(1) & 0xFF == 27:
       break
   cap.release()
